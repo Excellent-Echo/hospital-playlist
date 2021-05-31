f58b5001cff7c987ce7b1eb24cbfc3be7bb205ae
ammarteuku05 <ammarteuku09@gmail.com>
2021-05-31 19:47:48 +0700

    update migration

--- migration/entity.go
+++ migration/entity.go
@@ -3,16 +3,16 @@ package migration
 import "time"
 
 type User struct {
-	ID          int          `gorm:"primaryKey" json:"id"`
-	NamaLengkap string       `json:"nama_lengkap"`
-	Email       string       `json:"email"`
-	Password    string       `json:"-"`
-	CreatedAt   time.Time    `json:"created_at"`
-	UpdatedAt   time.Time    `json:"updated_at"`
-	DeletedAt   time.Time    `gorm:"index" json:"-"`
-	Specialists []Specialist `gorm:"foreignKey:UserID" json:"spesialis"`
-	Medicines   []Medicine   `gorm:"foreignKey:UserID" json:"medicine"`
-	Rooms       []Room       `gorm:"foreignKey:UserID" json:"room"`
+	ID          int       `gorm:"primaryKey" json:"id"`
+	NamaLengkap string    `json:"nama_lengkap"`
+	Email       string    `json:"email"`
+	Password    string    `json:"-"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+	DeletedAt   time.Time `gorm:"index" json:"-"`
+	// Specialists []Specialist `gorm:"foreignKey:UserID" json:"spesialis"`
+	// Medicines   []Medicine   `gorm:"foreignKey:UserID" json:"medicine"`
+	// Rooms       []Room       `gorm:"foreignKey:UserID" json:"room"`
 }
 
 type UserProfile struct {
